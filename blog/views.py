28f1c4c040c5701f86d39e7ae2660ce5aff49a20
edsmartin7 <edsmartin7@gmail.com>
2017-01-10 23:45:52 -0500

    gitignore is working

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.utils import timezone
@@ -9,5 +10,8 @@ def post_list(request):
    posts = Post.objects.all()
    return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+   post = get_object_or_404(Post, pk=pk)
+   return render(request, 'blog/post_detail.html', {'post': post})
 
 
